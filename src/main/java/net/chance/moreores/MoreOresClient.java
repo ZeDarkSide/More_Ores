e3e68b30b87c5e36d2612521da0ed8b0df533219
chance <chancemartin444@gmail.com>
2023-07-16 16:27:07 -0500

    added

---
+++ src/main/java/net/chance/moreores/MoreOresClient.java
@@ -0,0 +1,10 @@
+package net.chance.moreores;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoreOresClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/chance/moreores/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.chance.moreores.mixin;
+
+import net.chance.moreores.MoreOres;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		MoreOres.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
